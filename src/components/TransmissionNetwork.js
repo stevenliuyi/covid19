4edeeff16e698d21b410b71e17fcf1aea596a687
stevenliuyi <stevenliuyi@gmail.com>
2020-02-28 21:12:34 -0500

    database update

--- src/components/TransmissionNetwork.js
+++ src/components/TransmissionNetwork.js
@@ -83,7 +83,7 @@ export default class TransmissionNetwork extends Component {
             height: mapDimensions.height,
             highlightOpacity: 0.2,
             d3: {
-                gravity: -50,
+                gravity: -40,
                 linkLength: mapDimensions.width * 0.2
             },
             link: {
