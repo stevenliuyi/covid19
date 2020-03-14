752547ca62eac2c8f0c3f71e0be6d6e0904ca1f9
stevenliuyi <stevenliuyi@gmail.com>
2020-03-14 00:40:03 -0400

    database updates

--- src/scripts/data_processing_korea.js
+++ src/scripts/data_processing_korea.js
@@ -90,159 +90,159 @@ const splitCSV = function(string) {
     return matches
 }
 
-// recovered and death cases
-const data = fs.readFileSync(`${data_folder}/${line_list_file}`, 'utf8').split(/\r?\n/)
-
-// recovered cases
-const cured_index = 26
-
-assert(
-    data[0].split(',')[cured_index] === 'date_discharged',
-    `${data[0].split(',')[cured_index]} not matched with 'date_discharged'`
-)
-
-const curedData = data
-    .slice(1)
-    .filter((x) => splitCSV(x)[cured_index] !== '')
-    .sort((a, b) => (splitCSV(a)[cured_index] > splitCSV(b)[cured_index] ? 1 : -1))
-
-const latestDate = currentDate
-currentDate = first_date
-let nextDate = ''
-
-// initialization
-regions.forEach((region) => {
-    addDataToRegion(currentDate, currentDate, en2zh[region], 'curedCount', 0, true)
-})
-
-while (currentDate <= latestDate) {
-    const currentDateData = curedData.filter((x) => splitCSV(x)[cured_index] === currentDate)
-
-    currentDateData.forEach((x) => {
-        const lineSplit = splitCSV(x)
-        const city = lineSplit[2] !== '' ? lineSplit[2] : lineSplit[1]
-
-        let region = regions.includes(city)
-            ? city
-            : Object.keys(korea_cities).find((x) => korea_cities[x].includes(city))
-        if (region == null) {
-            console.log(`Cannot find the city '${city}' for case #${lineSplit[0]}!`)
-            region = 'Unknown Region'
-        }
-        addDataToRegion(currentDate, currentDate, en2zh[region], 'curedCount', 1, false)
-    })
-
-    // next day
-    nextDate = parseDate(currentDate)
-    nextDate.setDate(nextDate.getDate() + 1)
-    nextDate = nextDate.toISOString().slice(0, 10)
-
-    if (nextDate <= latestDate)
-        regions.forEach((region) => {
-            addDataToRegion(nextDate, currentDate, en2zh[region], 'curedCount', 0, false)
-        })
-
-    currentDate = nextDate
-}
-
-// death cases
-const dead_index = 27
-
-assert(
-    data[0].split(',')[dead_index] === 'date_death',
-    `${data[0].split(',')[dead_index]} not matched with 'date_death'`
-)
-const deadData = data
-    .slice(1)
-    .filter((x) => splitCSV(x)[dead_index] !== '')
-    .sort((a, b) => (splitCSV(a)[dead_index] > splitCSV(b)[dead_index] ? 1 : -1))
-
-currentDate = first_date
-nextDate = ''
-
-// initialization
-regions.forEach((region) => {
-    addDataToRegion(currentDate, currentDate, en2zh[region], 'deadCount', 0, true)
-})
-
-while (currentDate <= latestDate) {
-    const currentDateData = deadData.filter((x) => splitCSV(x)[dead_index] === currentDate)
-
-    currentDateData.forEach((x) => {
-        const lineSplit = splitCSV(x)
-        const city = lineSplit[2] !== '' ? lineSplit[2] : lineSplit[1]
-
-        let region = regions.includes(city)
-            ? city
-            : Object.keys(korea_cities).find((x) => korea_cities[x].includes(city))
-        if (region == null) {
-            console.log(`Cannot find the city '${city}' for case #${lineSplit[0]}!`)
-            region = 'Unknown Region'
-        }
-        addDataToRegion(currentDate, currentDate, en2zh[region], 'deadCount', 1, false)
-    })
-
-    // manually add missing cases
-    if (currentDate === '2020-02-19' && currentDateData.length === 0) {
-        addDataToRegion(currentDate, currentDate, en2zh['Unknown Region'], 'deadCount', 1, false)
-    }
-    if (currentDate === '2020-02-22' && currentDateData.length === 1) {
-        addDataToRegion(currentDate, currentDate, en2zh['Unknown Region'], 'deadCount', 1, false)
-    }
-
-    // next day
-    nextDate = parseDate(currentDate)
-    nextDate.setDate(nextDate.getDate() + 1)
-    nextDate = nextDate.toISOString().slice(0, 10)
-
-    if (nextDate <= latestDate)
-        regions.forEach((region) => {
-            addDataToRegion(nextDate, currentDate, en2zh[region], 'deadCount', 0, false)
-        })
-
-    currentDate = nextDate
-}
-
-// calculate cumulative counts
-currentDate = first_date
-output_korea['confirmedCount'] = {}
-output_korea['curedCount'] = {}
-output_korea['deadCount'] = {}
-
-while (currentDate <= latestDate) {
-    ;[ 'confirmedCount', 'curedCount', 'deadCount' ].forEach((metric) => {
-        output_korea[metric][currentDate] = Object.keys(output_korea)
-            .filter((x) => ![ 'confirmedCount', 'curedCount', 'deadCount', 'ENGLISH' ].includes(x))
-            .map((x) => output_korea[x][metric][currentDate])
-            .reduce((s, x) => s + x, 0)
-    })
-
-    // next day
-    nextDate = parseDate(currentDate)
-    nextDate.setDate(nextDate.getDate() + 1)
-    nextDate = nextDate.toISOString().slice(0, 10)
-    currentDate = nextDate
-}
-
-// check if the cumulative numbers match
-const cumulative_data = fs.readFileSync(`${data_folder}/${cumulative_numbers_file}`, 'utf8').split(/\r?\n/)
-cumulative_data.forEach((line, index) => {
-    if (index === 0) return
-    const lineSplit = splitCSV(line)
-    const date = lineSplit[0]
-    ;[ 'confirmedCount', 'curedCount', 'deadCount' ].forEach((metric, idx) => {
-        const count = parseInt(lineSplit[idx + 3], 10)
-        const sumOfRegions = output_korea[metric][date] ? output_korea[metric][date] : 0
-        if (count !== sumOfRegions) {
-            //console.log(
-            //    `${metric} on ${date} (${count}) doesn't match the sum of counts from all regions (${output_korea[
-            //        metric
-            //    ][date]}).`
-            //)
-            output_korea[metric][date] = count
-        }
-    })
-})
+//// recovered and death cases (outdated)
+//const data = fs.readFileSync(`${data_folder}/${line_list_file}`, 'utf8').split(/\r?\n/)
+//
+//// recovered cases
+//const cured_index = 26
+//
+//assert(
+//    data[0].split(',')[cured_index] === 'date_discharged',
+//    `${data[0].split(',')[cured_index]} not matched with 'date_discharged'`
+//)
+//
+//const curedData = data
+//    .slice(1)
+//    .filter((x) => splitCSV(x)[cured_index] !== '')
+//    .sort((a, b) => (splitCSV(a)[cured_index] > splitCSV(b)[cured_index] ? 1 : -1))
+//
+//const latestDate = currentDate
+//currentDate = first_date
+//let nextDate = ''
+//
+//// initialization
+//regions.forEach((region) => {
+//    addDataToRegion(currentDate, currentDate, en2zh[region], 'curedCount', 0, true)
+//})
+//
+//while (currentDate <= latestDate) {
+//    const currentDateData = curedData.filter((x) => splitCSV(x)[cured_index] === currentDate)
+//
+//    currentDateData.forEach((x) => {
+//        const lineSplit = splitCSV(x)
+//        const city = lineSplit[2] !== '' ? lineSplit[2] : lineSplit[1]
+//
+//        let region = regions.includes(city)
+//            ? city
+//            : Object.keys(korea_cities).find((x) => korea_cities[x].includes(city))
+//        if (region == null) {
+//            console.log(`Cannot find the city '${city}' for case #${lineSplit[0]}!`)
+//            region = 'Unknown Region'
+//        }
+//        addDataToRegion(currentDate, currentDate, en2zh[region], 'curedCount', 1, false)
+//    })
+//
+//    // next day
+//    nextDate = parseDate(currentDate)
+//    nextDate.setDate(nextDate.getDate() + 1)
+//    nextDate = nextDate.toISOString().slice(0, 10)
+//
+//    if (nextDate <= latestDate)
+//        regions.forEach((region) => {
+//            addDataToRegion(nextDate, currentDate, en2zh[region], 'curedCount', 0, false)
+//        })
+//
+//    currentDate = nextDate
+//}
+//
+//// death cases
+//const dead_index = 27
+//
+//assert(
+//    data[0].split(',')[dead_index] === 'date_death',
+//    `${data[0].split(',')[dead_index]} not matched with 'date_death'`
+//)
+//const deadData = data
+//    .slice(1)
+//    .filter((x) => splitCSV(x)[dead_index] !== '')
+//    .sort((a, b) => (splitCSV(a)[dead_index] > splitCSV(b)[dead_index] ? 1 : -1))
+//
+//currentDate = first_date
+//nextDate = ''
+//
+//// initialization
+//regions.forEach((region) => {
+//    addDataToRegion(currentDate, currentDate, en2zh[region], 'deadCount', 0, true)
+//})
+//
+//while (currentDate <= latestDate) {
+//    const currentDateData = deadData.filter((x) => splitCSV(x)[dead_index] === currentDate)
+//
+//    currentDateData.forEach((x) => {
+//        const lineSplit = splitCSV(x)
+//        const city = lineSplit[2] !== '' ? lineSplit[2] : lineSplit[1]
+//
+//        let region = regions.includes(city)
+//            ? city
+//            : Object.keys(korea_cities).find((x) => korea_cities[x].includes(city))
+//        if (region == null) {
+//            console.log(`Cannot find the city '${city}' for case #${lineSplit[0]}!`)
+//            region = 'Unknown Region'
+//        }
+//        addDataToRegion(currentDate, currentDate, en2zh[region], 'deadCount', 1, false)
+//    })
+//
+//    // manually add missing cases
+//    if (currentDate === '2020-02-19' && currentDateData.length === 0) {
+//        addDataToRegion(currentDate, currentDate, en2zh['Unknown Region'], 'deadCount', 1, false)
+//    }
+//    if (currentDate === '2020-02-22' && currentDateData.length === 1) {
+//        addDataToRegion(currentDate, currentDate, en2zh['Unknown Region'], 'deadCount', 1, false)
+//    }
+//
+//    // next day
+//    nextDate = parseDate(currentDate)
+//    nextDate.setDate(nextDate.getDate() + 1)
+//    nextDate = nextDate.toISOString().slice(0, 10)
+//
+//    if (nextDate <= latestDate)
+//        regions.forEach((region) => {
+//            addDataToRegion(nextDate, currentDate, en2zh[region], 'deadCount', 0, false)
+//        })
+//
+//    currentDate = nextDate
+//}
+//
+//// calculate cumulative counts
+//currentDate = first_date
+//output_korea['confirmedCount'] = {}
+//output_korea['curedCount'] = {}
+//output_korea['deadCount'] = {}
+//
+//while (currentDate <= latestDate) {
+//    ;[ 'confirmedCount', 'curedCount', 'deadCount' ].forEach((metric) => {
+//        output_korea[metric][currentDate] = Object.keys(output_korea)
+//            .filter((x) => ![ 'confirmedCount', 'curedCount', 'deadCount', 'ENGLISH' ].includes(x))
+//            .map((x) => output_korea[x][metric][currentDate])
+//            .reduce((s, x) => s + x, 0)
+//    })
+//
+//    // next day
+//    nextDate = parseDate(currentDate)
+//    nextDate.setDate(nextDate.getDate() + 1)
+//    nextDate = nextDate.toISOString().slice(0, 10)
+//    currentDate = nextDate
+//}
+//
+//// check if the cumulative numbers match
+//const cumulative_data = fs.readFileSync(`${data_folder}/${cumulative_numbers_file}`, 'utf8').split(/\r?\n/)
+//cumulative_data.forEach((line, index) => {
+//    if (index === 0) return
+//    const lineSplit = splitCSV(line)
+//    const date = lineSplit[0]
+//    ;[ 'confirmedCount', 'curedCount', 'deadCount' ].forEach((metric, idx) => {
+//        const count = parseInt(lineSplit[idx + 3], 10)
+//        const sumOfRegions = output_korea[metric][date] ? output_korea[metric][date] : 0
+//        if (count !== sumOfRegions) {
+//            //console.log(
+//            //    `${metric} on ${date} (${count}) doesn't match the sum of counts from all regions (${output_korea[
+//            //        metric
+//            //    ][date]}).`
+//            //)
+//            output_korea[metric][date] = count
+//        }
+//    })
+//})
 
 fs.writeFileSync(`public/data/korea.json`, JSON.stringify(output_korea))
 
