6be89773641557b46cda0c03f629021504543eee
stevenliuyi <stevenliuyi@gmail.com>
2020-03-23 19:03:08 -0400

    seems layout changed; use hover/touch to show daily cases

--- src/components/MainCounts.js
+++ src/components/MainCounts.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import { metricText, getDataFromRegion, previousDay } from '../utils/utils'
+import { isMobile, isIPad13 } from 'react-device-detect'
 import i18n from '../data/i18n.yml'
 
 export default class MainCounts extends Component {
+    state = {
+        showNewCases: {
+            confirmedCount: false,
+            deadCount: false,
+            curedCount: false
+        }
+    }
+
+    toggleView = (metric) => {
+        let showNewCases = this.state.showNewCases
+        showNewCases[metric] = !showNewCases[metric]
+        this.setState({ showNewCases })
+    }
+
     render() {
         const { data, currentRegion, date, lang, fullPlot } = this.props
         if (data == null) return <div />
@@ -15,25 +30,35 @@ export default class MainCounts extends Component {
                             Object.keys(getDataFromRegion(data, currentRegion)[metric]).length > 0
                                 ? getDataFromRegion(data, currentRegion)[metric][date]
                                 : '—'
-                        const preDate = previousDay(date, "2019-09-01", "2050-01-01")
+                        const preDate = previousDay(date, '2019-09-01', '2050-01-01')
 
-                        const preCount = 
+                        const preCount =
                             Object.keys(getDataFromRegion(data, currentRegion)[metric]).length > 0
-                            ? getDataFromRegion(data, currentRegion)[metric][preDate]
-                            : 0
+                                ? getDataFromRegion(data, currentRegion)[metric][preDate]
+                                : 0
                         let diff = 0
-                        if (parseInt(count) != null && parseInt(preCount) != null) 
+                        if (parseInt(count) != null && parseInt(preCount) != null)
                             diff = parseInt(count) - parseInt(preCount)
-                        
+
                         return (
-                            <div key={`${metric}-number`} className="count-wrap">
-                                <div className={`${metric}-count`}>{count ? count : 0}</div>
-                                <div className="count-title">{metricText[metric][lang]}</div>
-
-                                <div className={`${metric}-count`}>{diff}</div>
-                                <div className="count-title">{i18n.NEWCASE[lang]}</div>
+                            <div
+                                key={`${metric}-number`}
+                                className="count-wrap"
+                                onMouseEnter={() => (isMobile || isIPad13 ? null : this.toggleView(metric))}
+                                onMouseLeave={() => (isMobile || isIPad13 ? null : this.toggleView(metric))}
+                                onTouchStart={() => this.toggleView(metric)}
+                            >
+                                {!this.state.showNewCases[metric] ? (
+                                    <div className={`${metric}-count`}>{count ? count : 0}</div>
+                                ) : (
+                                    <div className={`${metric}-count`}>{diff}</div>
+                                )}
+                                {!this.state.showNewCases[metric] ? (
+                                    <div className="count-title">{metricText[metric][lang]}</div>
+                                ) : (
+                                    <div className="count-title">{i18n.NEWCASE[lang]}</div>
+                                )}
                             </div>
-
                         )
                     })}
             </div>
