f84467a81e643f8065d8d6f1c9c6d1cd6e948910
stevenliuyi <stevenliuyi@gmail.com>
2020-04-01 10:12:45 -0400

    fix

--- scripts/data_processing_norway.js
+++ scripts/data_processing_norway.js
@@ -23,7 +23,7 @@ data.forEach((line, index) => {
     const lineSplit = line.split(',')
 
     let regionEnglish = lineSplit[1]
-    if (regionEnglish === 'Unknown county') return
+    if (regionEnglish === 'Unknown county' || regionEnglish === 'Unknown') return
 
     const confirmedCount = parseInt(lineSplit[2], 10)
     const date = lineSplit[3].slice(0, 10)
