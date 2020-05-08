6f2427aa2bd05bc3bc9528f8a3dadf639d166f54
stevenliuyi <stevenliuyi@gmail.com>
2020-05-07 22:59:08 -0400

    use new dataset for Spain

--- scripts/data_processing_spain.js
+++ scripts/data_processing_spain.js
@@ -2,11 +2,7 @@ const fs = require('fs')
 const assert = require('assert')
 
 const data_folder = 'data/spain-data/COVID 19'
-const data_files = {
-    confirmedCount: 'ccaa_covid19_casos.csv',
-    deadCount: 'ccaa_covid19_fallecidos.csv',
-    curedCount: 'ccaa_covid19_altas.csv'
-}
+const data_file = 'ccaa_covid19_datos_isciii.csv'
 
 // translations
 let en2zh = JSON.parse(fs.readFileSync('data/map-translations/en2zh.json'))
@@ -22,44 +18,37 @@ output_spain = {
     deadCount: {},
     curedCount: {}
 }
-;[ 'confirmedCount', 'curedCount', 'deadCount' ].forEach((metric) => {
-    const data = fs.readFileSync(`${data_folder}/${data_files[metric]}`, 'utf8').split(/\r?\n/)
 
-    let dates = []
-    data.forEach((line, index) => {
-        const lineSplit = line.split(',')
-        if (index === 0) {
-            // dates
-            dates = lineSplit.slice(2)
-        } else {
-            if (lineSplit.length === 1) return
+const data = fs.readFileSync(`${data_folder}/${data_file}`, 'utf8').split(/\r?\n/)
 
-            let regionEnglish = lineSplit[1]
-            if (regionEnglish in name_changes) regionEnglish = name_changes[regionEnglish]
-            const region = en2zh[regionEnglish]
+data.forEach((line, index) => {
+    const lineSplit = line.split(',')
+    if (index === 0 || line === '') return
 
-            assert(regionEnglish === 'Total' || region != null, `${regionEnglish} does not exist!`)
-            const counts = lineSplit.slice(2).map((x) => parseInt(x, 10))
+    const date = lineSplit[0]
+    let regionEnglish = lineSplit[2]
+    if (regionEnglish in name_changes) regionEnglish = name_changes[regionEnglish]
 
-            if (regionEnglish !== 'Total' && !(region in output_spain)) {
-                output_spain[region] = {
-                    ENGLISH: regionEnglish,
-                    confirmedCount: {},
-                    curedCount: {},
-                    deadCount: {}
-                }
-            }
+    const confirmedCount = parseInt(lineSplit[3], 10)
+    const deadCount = parseInt(lineSplit[8], 10)
+    const curedCount = parseInt(lineSplit[9], 10)
 
-            dates.forEach((date, i) => {
-                assert(!isNaN(new Date(date)), `Date ${date} is not valid!`)
-                if (regionEnglish !== 'Total') {
-                    output_spain[region][metric][date] = counts[i]
-                } else {
-                    output_spain[metric][date] = counts[i]
-                }
-            })
+    assert(!isNaN(new Date(date)), `Date ${date} is not valid!`)
+
+    const region = en2zh[regionEnglish]
+    assert(region != null, `${regionEnglish} does not exist!`)
+
+    if (!(region in output_spain)) {
+        output_spain[region] = {
+            ENGLISH: regionEnglish,
+            confirmedCount: {},
+            curedCount: {},
+            deadCount: {}
         }
-    })
+    }
+    if (!isNaN(confirmedCount)) output_spain['confirmedCount'][date] = confirmedCount
+    if (!isNaN(deadCount)) output_spain['deadCount'][date] = deadCount
+    if (!isNaN(curedCount)) output_spain['curedCount'][date] = curedCount
 })
 
 fs.writeFileSync(`public/data/spain.json`, JSON.stringify(output_spain))
