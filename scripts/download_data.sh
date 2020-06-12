18e221ab9df9769fea4fe6ce4aa649f61c339aad
stevenliuyi <stevenliuyi@gmail.com>
2020-06-12 10:43:23 -0400

    clean up

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,10 +5,6 @@ mkdir -p data/korea-data
 wget -q --no-check-certificate 'https://docs.google.com/spreadsheets/d/1nKRkOwnGV7RgsMnsYE6l96u4xxl3ZaNiTluPKEPaWm8/export?gid=898304475&format=csv' -O data/korea-data/geo_distribution.csv
 wget -q --no-check-certificate 'https://docs.google.com/spreadsheets/d/1nKRkOwnGV7RgsMnsYE6l96u4xxl3ZaNiTluPKEPaWm8/export?gid=306770783&format=csv' -O data/korea-data/cumulative_numbers.csv
 
-# download data files for India cases
-mkdir -p data/india-data
-wget -q --no-check-certificate 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pub?output=csv' -O data/india-data/raw.csv
-
 # download data file for Indonesia cases
 mkdir -p data/indonesia-data
 wget -q --no-check-certificate 'https://docs.google.com/spreadsheets/d/1sgiz8x71QyIVJZQguYtG9n6xBEKdM4fXuDs_d8zKOmY/export?gid=83750310&format=csv' -O data/indonesia-data/data_provinsi.csv
@@ -24,6 +20,10 @@ wget -q "https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_cov
 # download data for Latvia
 mkdir -p data/latvia-data
 wget -q "https://data.gov.lv/dati/dataset/e150cc9a-27c1-4920-a6c2-d20d10469873/resource/492931dd-0012-46d7-b415-76fe0ec7c216/download/covid_19_pa_adm_terit.csv" -O data/latvia-data/raw.csv
+
+# Ethiopia
+# data source: https://covid19.qulph.com/
+# https://docs.google.com/spreadsheets/d/1wFUxclZN5IZgnKXlXol2TIrWZ3UW2SLnDwQzZyRW11k/edit#gid=771389376
 
 # data folder
 mkdir -p public/data
