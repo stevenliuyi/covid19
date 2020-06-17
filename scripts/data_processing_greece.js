bba5fa7aee2a5bc853f770c95e736740a22edf57
stevenliuyi <stevenliuyi@gmail.com>
2020-06-16 20:28:48 -0400

    fix

--- scripts/data_processing_greece.js
+++ scripts/data_processing_greece.js
@@ -2,9 +2,12 @@ const fs = require('fs')
 const assert = require('assert')
 
 const data_folder = 'data/greece-data/COVID-19'
+//const data_files = {
+//    confirmedCount: 'regions_greece_cases.csv',
+//    deadCount: 'regions_greece_deaths.csv'
+//}
 const data_files = {
-    confirmedCount: 'regions_greece_cases.csv',
-    deadCount: 'regions_greece_deaths.csv'
+    confirmedCount: 'regions_greece_cases.csv'
 }
 
 // translations
