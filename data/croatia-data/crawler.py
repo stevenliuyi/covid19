62a1fe450064d7824295eea381ceb7fda6509c46
stevenliuyi <stevenliuyi@gmail.com>
2021-02-26 03:36:52 -0500

    bypass Cloudflare protection

--- data/croatia-data/crawler.py
+++ data/croatia-data/crawler.py
@@ -1,15 +1,35 @@
 import requests
+from bs4 import BeautifulSoup
 import json
 
-url = 'https://www.koronavirus.hr/json/?action=po_danima_zupanijama'
+url = 'https://koronavirus.hr/json/?action=po_danima_zupanijama'
 
-data = requests.get(url=url).json()
+headers = {'Content-Type': 'application/json'}
+request_data = {
+    'cmd': 'request.get',
+    'url': url,
+    'userAgent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
+    'maxTimeout': 60000
+}
 
-data = json.dumps(
-    data,
-    indent=2,
-    ensure_ascii=False,
-)
+try:
+    response = requests.post(url='http://localhost:8191/v1',
+                             data=json.dumps(request_data),
+                             headers=headers)
+    html_txt = response.json()['solution']['response']
+    soup = BeautifulSoup(html_txt, 'html.parser')
+    data = soup.find('pre').text
+
+    data = json.loads(data)
+    data = json.dumps(
+        data,
+        indent=2,
+        ensure_ascii=False,
+    )
+except Exception as e:
+    print('Error ocurred when scraping Pakistan data!')
+    print(e)
 
 f = open('data/croatia-data/raw.json', 'w')
 f.write(data)
